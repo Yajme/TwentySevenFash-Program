5211ab1aa2be5a196e5526aa5489775d2571ec30
yajme <spartan.emjayrongavilla@gmail.com>
2022-10-17 22:58:41 +0800

    Files more organized

--- DashboardFrm/dashboardInventory.Designer.vb
+++ DashboardFrm/dashboardInventory.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class ItemsByJM
+Partial Class dashboardInventory
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -23,10 +23,10 @@ Partial Class ItemsByJM
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.txtItemID = New System.Windows.Forms.TextBox()
-        Me.Label2 = New System.Windows.Forms.Label()
         Me.btnSEARCH = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtItemID = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.Panel1.SuspendLayout()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -38,63 +38,69 @@ Partial Class ItemsByJM
         Me.Panel1.Controls.Add(Me.Label2)
         Me.Panel1.Controls.Add(Me.txtItemID)
         Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Location = New System.Drawing.Point(32, 108)
+        Me.Panel1.Location = New System.Drawing.Point(37, 144)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(213, 162)
+        Me.Panel1.Size = New System.Drawing.Size(243, 216)
         Me.Panel1.TabIndex = 0
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(37, 57)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(48, 15)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Item ID:"
-        '
-        'txtItemID
-        '
-        Me.txtItemID.Location = New System.Drawing.Point(100, 54)
-        Me.txtItemID.Name = "txtItemID"
-        Me.txtItemID.Size = New System.Drawing.Size(100, 23)
-        Me.txtItemID.TabIndex = 1
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(60, 18)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(80, 15)
-        Me.Label2.TabIndex = 2
-        Me.Label2.Text = "ITEM SEARCH"
         '
         'btnSEARCH
         '
-        Me.btnSEARCH.Location = New System.Drawing.Point(65, 103)
+        Me.btnSEARCH.Location = New System.Drawing.Point(74, 137)
+        Me.btnSEARCH.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.btnSEARCH.Name = "btnSEARCH"
-        Me.btnSEARCH.Size = New System.Drawing.Size(75, 23)
+        Me.btnSEARCH.Size = New System.Drawing.Size(86, 31)
         Me.btnSEARCH.TabIndex = 3
         Me.btnSEARCH.Text = "SEARCH"
         Me.btnSEARCH.UseVisualStyleBackColor = True
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(69, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(101, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "ITEM SEARCH"
+        '
+        'txtItemID
+        '
+        Me.txtItemID.Location = New System.Drawing.Point(114, 72)
+        Me.txtItemID.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtItemID.Name = "txtItemID"
+        Me.txtItemID.Size = New System.Drawing.Size(114, 27)
+        Me.txtItemID.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(42, 76)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Item ID:"
+        '
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(282, 63)
+        Me.DataGridView1.Location = New System.Drawing.Point(322, 84)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
         Me.DataGridView1.RowTemplate.Height = 25
-        Me.DataGridView1.Size = New System.Drawing.Size(477, 324)
+        Me.DataGridView1.Size = New System.Drawing.Size(545, 432)
         Me.DataGridView1.TabIndex = 1
         '
-        'ItemsByJM
+        'dashboardInventory
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(914, 600)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Panel1)
-        Me.Name = "ItemsByJM"
-        Me.Text = "ItemsByJM"
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "dashboardInventory"
+        Me.Text = "dashboardInventory"
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
