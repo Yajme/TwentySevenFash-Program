dd9d954d3670173d4dd0083a85abb4d7db10922c
yajme <spartan.emjayrongavilla@gmail.com>
2022-10-22 12:12:19 +0800

    dashboard pos started

--- dashboardPOS.vb
+++ dashboardPOS.vb
@@ -1,3 +1,7 @@
-﻿Public Class dashboardPOS
+﻿Imports System.Data.SqlClient
+
+Public Class dashboardPOS
+    Dim con As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\TwentySevenFash-Program\TwentySevenFash.mdf;Integrated Security=True")
+    Dim cmd As New SqlCommand("select * from items ")
 
 End Class
