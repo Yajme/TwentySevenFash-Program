1b021e1d0da4fd51b7b0c04e11a25bea6d8b021e
phmac <phmac@MSI>
2022-10-22 12:03:05 +0800

    haha

--- ItemsByBelleAndJM.vb
+++ ItemsByBelleAndJM.vb
@@ -1,7 +1,12 @@
-﻿Public Class ItemsByBelleAndJM
+﻿Imports System.Data.SqlClient
+
+Public Class ItemsByBelleAndJM
     Dim con As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\TwentySevenFash-Program\TwentySevenFash.mdf;Integrated Security=True")
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (ComboBox1.SelectedItem = "ID") Then
+            Dim cmd As New SqlCommand("select * from Items where ")
 
+        End If
     End Sub
 End Class
