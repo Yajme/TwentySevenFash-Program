6b4caa994636c952d5337e84f127cadab9686292
phmac <phmac@MSI>
2022-10-23 16:08:07 +0800

    jm

--- ItemsByBelleAndJM.vb
+++ ItemsByBelleAndJM.vb
@@ -2,7 +2,7 @@
 
 Public Class ItemsByBelleAndJM
     Dim con As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\TwentySevenFash-Program\TwentySevenFash.mdf;Integrated Security=True")
-
+    Dim qry As String = ""
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         If (ComboBox1.SelectedItem = "ID") Then
             Dim cmd As New SqlCommand("select * from Items where ID = @id", con)
@@ -10,5 +10,12 @@ Public Class ItemsByBelleAndJM
             cmd.Parameters.Add("@id", SqlDbType.VarChar).Value = txtSearch.Text
 
         End If
+    End Sub
+
+    Private Sub ItemsByBelleAndJM_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Bind()
+    End Sub
+    Private Sub Bind()
+
     End Sub
 End Class
