8f0c159b27c704a7c7f9d9ad7a34abb96280d59b
phmac <phmac@MSI>
2022-10-24 17:59:44 +0800

    ey

--- Login.vb
+++ Login.vb
@@ -30,7 +30,7 @@ Public Class Login
 
             Dim table As New DataTable()
 
-            sda.Fill(table)
+            sda.Fill(table)e
             If table.Rows.Count() <= 0 Then
                 MsgBox("Enter Valid username and password", MsgBoxStyle.OkOnly, "Error")
             End If
