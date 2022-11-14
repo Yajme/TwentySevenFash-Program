5be31a3cd304ac18f86bfdec55a0b6d24657e4d0
Allen Ramos <ramosalleneid01@gmail.com>
2022-11-14 13:48:04 +0800

    sales

--- dashboardSales.Designer.vb
+++ dashboardSales.Designer.vb
@@ -28,7 +28,15 @@ Partial Class dashboardSales
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.expdLabel = New System.Windows.Forms.Label()
+        Me.ntcmLabel = New System.Windows.Forms.Label()
+        Me.rvnLabel = New System.Windows.Forms.Label()
+        Me.sldLabel = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
         Me.SuspendLayout()
         '
         'Panel1
@@ -37,24 +45,28 @@ Partial Class dashboardSales
         Me.Panel1.BackgroundImage = CType(resources.GetObject("Panel1.BackgroundImage"), System.Drawing.Image)
         Me.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.rvnLabel)
+        Me.Panel1.Controls.Add(Me.sldLabel)
+        Me.Panel1.Controls.Add(Me.ntcmLabel)
+        Me.Panel1.Controls.Add(Me.expdLabel)
         Me.Panel1.Controls.Add(Me.Label4)
         Me.Panel1.Controls.Add(Me.Label3)
         Me.Panel1.Controls.Add(Me.Label2)
         Me.Panel1.Controls.Add(Me.Label1)
         Me.Panel1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Location = New System.Drawing.Point(29, 37)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(302, 290)
+        Me.Panel1.Size = New System.Drawing.Size(574, 377)
         Me.Panel1.TabIndex = 0
         '
         'Label4
         '
         Me.Label4.AutoSize = True
         Me.Label4.BackColor = System.Drawing.Color.Transparent
-        Me.Label4.Font = New System.Drawing.Font("Impact", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label4.Location = New System.Drawing.Point(16, 125)
+        Me.Label4.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(16, 273)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(72, 17)
+        Me.Label4.Size = New System.Drawing.Size(109, 19)
         Me.Label4.TabIndex = 3
         Me.Label4.Text = "Net Income:"
         '
@@ -62,10 +74,10 @@ Partial Class dashboardSales
         '
         Me.Label3.AutoSize = True
         Me.Label3.BackColor = System.Drawing.Color.Transparent
-        Me.Label3.Font = New System.Drawing.Font("Impact", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label3.Location = New System.Drawing.Point(16, 90)
+        Me.Label3.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(16, 177)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(127, 17)
+        Me.Label3.Size = New System.Drawing.Size(192, 19)
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "Number of Sold Items:"
         '
@@ -73,10 +85,10 @@ Partial Class dashboardSales
         '
         Me.Label2.AutoSize = True
         Me.Label2.BackColor = System.Drawing.Color.Transparent
-        Me.Label2.Font = New System.Drawing.Font("Impact", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label2.Location = New System.Drawing.Point(16, 53)
+        Me.Label2.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(16, 94)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(58, 17)
+        Me.Label2.Size = New System.Drawing.Size(86, 19)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Revenue:"
         '
@@ -84,12 +96,94 @@ Partial Class dashboardSales
         '
         Me.Label1.AutoSize = True
         Me.Label1.BackColor = System.Drawing.Color.Transparent
-        Me.Label1.Font = New System.Drawing.Font("Impact", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label1.Location = New System.Drawing.Point(16, 11)
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(16, 14)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(112, 17)
+        Me.Label1.Size = New System.Drawing.Size(170, 19)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Total Expenditures:"
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Transparent
+        Me.Panel2.BackgroundImage = CType(resources.GetObject("Panel2.BackgroundImage"), System.Drawing.Image)
+        Me.Panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.Label5)
+        Me.Panel2.Controls.Add(Me.Label8)
+        Me.Panel2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Panel2.Location = New System.Drawing.Point(608, 85)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(291, 281)
+        Me.Panel2.TabIndex = 4
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.BackColor = System.Drawing.Color.Transparent
+        Me.Label8.Font = New System.Drawing.Font("Impact", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label8.Location = New System.Drawing.Point(78, 61)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(140, 23)
+        Me.Label8.TabIndex = 0
+        Me.Label8.Text = "Business Result:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Gill Sans Ultra Bold", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.ImageAlign = System.Drawing.ContentAlignment.TopRight
+        Me.Label5.Location = New System.Drawing.Point(27, 101)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(235, 117)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "The Business" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Have" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Profits!"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'expdLabel
+        '
+        Me.expdLabel.AutoSize = True
+        Me.expdLabel.BackColor = System.Drawing.Color.Transparent
+        Me.expdLabel.Font = New System.Drawing.Font("Franklin Gothic Heavy", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.expdLabel.Location = New System.Drawing.Point(59, 44)
+        Me.expdLabel.Name = "expdLabel"
+        Me.expdLabel.Size = New System.Drawing.Size(149, 34)
+        Me.expdLabel.TabIndex = 4
+        Me.expdLabel.Text = "expdLabel"
+        '
+        'ntcmLabel
+        '
+        Me.ntcmLabel.AutoSize = True
+        Me.ntcmLabel.BackColor = System.Drawing.Color.Transparent
+        Me.ntcmLabel.Font = New System.Drawing.Font("Franklin Gothic Heavy", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.ntcmLabel.Location = New System.Drawing.Point(59, 312)
+        Me.ntcmLabel.Name = "ntcmLabel"
+        Me.ntcmLabel.Size = New System.Drawing.Size(149, 34)
+        Me.ntcmLabel.TabIndex = 5
+        Me.ntcmLabel.Text = "ntcmLabel"
+        '
+        'rvnLabel
+        '
+        Me.rvnLabel.AutoSize = True
+        Me.rvnLabel.BackColor = System.Drawing.Color.Transparent
+        Me.rvnLabel.Font = New System.Drawing.Font("Franklin Gothic Heavy", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.rvnLabel.Location = New System.Drawing.Point(59, 129)
+        Me.rvnLabel.Name = "rvnLabel"
+        Me.rvnLabel.Size = New System.Drawing.Size(126, 34)
+        Me.rvnLabel.TabIndex = 5
+        Me.rvnLabel.Text = "rvnLabel"
+        '
+        'sldLabel
+        '
+        Me.sldLabel.AutoSize = True
+        Me.sldLabel.BackColor = System.Drawing.Color.Transparent
+        Me.sldLabel.Font = New System.Drawing.Font("Franklin Gothic Heavy", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.sldLabel.Location = New System.Drawing.Point(59, 217)
+        Me.sldLabel.Name = "sldLabel"
+        Me.sldLabel.Size = New System.Drawing.Size(124, 34)
+        Me.sldLabel.TabIndex = 5
+        Me.sldLabel.Text = "sldLabel"
         '
         'dashboardSales
         '
@@ -98,6 +192,7 @@ Partial Class dashboardSales
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(906, 451)
+        Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
         Me.DoubleBuffered = True
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
@@ -105,6 +200,8 @@ Partial Class dashboardSales
         Me.Text = "dashboardSales"
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -114,4 +211,11 @@ Partial Class dashboardSales
     Friend WithEvents Label3 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label1 As Label
+    Friend WithEvents rvnLabel As Label
+    Friend WithEvents sldLabel As Label
+    Friend WithEvents ntcmLabel As Label
+    Friend WithEvents expdLabel As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label8 As Label
 End Class
--- dashboardSales.vb
+++ dashboardSales.vb
@@ -1,3 +1,59 @@
-﻿Public Class dashboardSales
+﻿Imports System.Data.SqlClient
 
+Public Class dashboardSales
+    Dim con As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\TwentySevenFash-Program\TwentySevenFash.mdf;Integrated Security=True")
+    Dim cmd As SqlCommand
+    Dim cmdd As SqlCommand
+
+    Dim dr As SqlDataReader
+    Dim addi As SqlDataReader
+
+
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub dashboardSales_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        con.Open()
+
+
+        Dim val1 As Integer
+        Dim val2 As Integer
+
+        Dim expenses As Double
+
+
+        val1 = 1
+
+        val2 = 0
+
+        While val1 <= 3
+            Dim doman As String = "Select DateofTransaction,Id,Expenses,Sales,Profit,numberofsoldshirts from Sales where Id=" & val1
+            cmdd = New SqlCommand(doman, con)
+            cmdd.Parameters.AddWithValue("Id", val1)
+            addi = cmdd.ExecuteReader()
+            If (addi.Read()) Then
+                expenses = expenses + addi("Expenses")
+
+            End If
+
+            expdLabel.Text = "PHP" & expenses
+
+            val1 = val1 + 1
+            addi.Close()
+        End While
+
+
+        con.Close()
+
+
+
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles expdLabel.Click
+
+    End Sub
 End Class
