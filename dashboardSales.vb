4a56829fcfc0b046b603e3f3be5a224a5e27cebf
phmac <phmac@MSI>
2022-11-20 13:10:24 +0800

    Ronald pogi

--- dashboardSales.vb
+++ dashboardSales.vb
@@ -38,4 +38,8 @@ Public Class dashboardSales
         con.Close()
 
     End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
 End Class
